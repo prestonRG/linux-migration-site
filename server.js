27864532d23e04a89b6d2ffc0259e6c9f0f2572d
Preston <80187314+prestonRG@users.noreply.github.com>
2025-12-08 18:34:54 -0500

    added get and post functions and pictures
    added get and post to/from api/items. also added required images for the project

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// fileName : server.js
 // Express.js
 const express = require('express');
 const path = require('path');
@@ -10,6 +9,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Add a GET route for /api/items
+app.get('/api/items', (req, res) => {
+    res.json({ message: 'GET api/items' });
+});
+
+// Add a POST route for /api/itmes
+app.post('/api/items', (req, res) => {
+    res.send('POST api/items');
 });
 
 // Specifying the port and starting the server
