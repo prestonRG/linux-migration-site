4cb7c6ae52ed78524c18779717f4ee5557d3e8ca
Preston <80187314+prestonRG@users.noreply.github.com>
2025-12-08 19:02:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Code on this page sourced from https://medium.com/@ibrahimhz/creating-your-first-backend-with-node-js-step-by-step-guide-892769af4cb0
+
 // Express.js
 const express = require('express');
 const path = require('path');
@@ -13,16 +15,23 @@ app.get('/', (req, res) => {
 
 // Add a GET route for /api/items
 app.get('/api/items', (req, res) => {
-    res.json({ message: 'GET api/items' });
+    res.json({ message: 'GET sample item' });
 });
 
 // Add a POST route for /api/itmes
 app.post('/api/items', (req, res) => {
-    res.send('POST api/items');
+    res.send('POST sample item');
 });
 
 // Specifying the port and starting the server
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+// server.js from project help page
+const GREETING = process.env.GREETING || 'Hello, Linux!';
+
+app.get('/api/greet', (req, res) => {
+  res.json({ message: GREETING });
 });
